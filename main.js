b372911b27a81edd655198dc403877198428386e
Joachim Ford <contact@joachimford.uk>
2025-08-19 09:26:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 'use strict'
+const VP_add = (x,y) => {if (VP_DATA[x] == undefined) VP_DATA[x] = y}
 
-const VP_CURSOR_SIZE = 100
-const VP_CURSOR_OFT_X = 20
-const VP_CURSOR_OFT_Y = 30
-const VP_CURSOR_OFT_A = 2
+const VP_DATA = document.documentElement.websitePresenterData
+VP_add('cursorTilt', false)
+VP_add('cursorSize', 1)
+VP_add('cursorAng', 2)
+VP_add('cursorRotSpeed', 1)
+VP_add('hideCursor', true)
+VP_add('smoothScroll', 1)
+VP_add('scrollMomentum', .99)
+VP_add('scrollForce', .8)
+VP_add('hideCursor', false)
+VP_add('hideOnPause', false)
+
+const VP_CURSOR_SIZE = 100 * VP_DATA.cursorSize
+const VP_CURSOR_OFT_X = 10
+const VP_CURSOR_OFT_Y = 10
+const VP_CURSOR_OFT_A = VP_DATA.cursorAng
 
 const VP_PRESS_SHARP = .1
 const VP_MOVE_SHARP = .05
-const VP_ANG_SHARP = .05
+const VP_ANG_SHARP = .05 * VP_DATA.cursorRotSpeed
 
 const VP_ANCHOR_SCROLL_SHARP = .05
 
-const VP_SCROLL_FORCE = .8
-const VP_SCROLL_MOMENTUM = .99
+const VP_SCROLL_FORCE = VP_DATA.scrollForce
+const VP_SCROLL_MOMENTUM = VP_DATA.scrollMomentum
 
 const VP_MOUSE = {x: 0, y: 0, lastX: 0, lastY: 0}
 const VP_CURSOR = {x: 0, y: 0, a: 0, lastA: 0, pressed: false, pressAmt: 1}
@@ -92,7 +105,8 @@ function VP_UPDATE() {
 
     const continuousTarget = VP_CURSOR.a + angleDiff
 
-    VP_CURSOR.a += (continuousTarget - VP_CURSOR.a) * VP_ANG_SHARP
+    if (!VP_DATA.cursorTilt) VP_CURSOR.a += (continuousTarget - VP_CURSOR.a) * VP_ANG_SHARP
+    else VP_CURSOR.a = VP_DATA.cursorAng + (dx * VP_DATA.cursorRotSpeed * .001)
 
     // -- Apply cursor transforms ---
     VP_SVG.style.transform = `
@@ -105,16 +119,19 @@ function VP_UPDATE() {
         const dist = VP_SCROLL_GOAL - VP_SCROLL_POS
 
         VP_SCROLL_POS += dist * VP_ANCHOR_SCROLL_SHARP
+        VP_SMOOTH_SCROLL_POS = VP_SCROLL_POS
         VP_SCROLL_SPEED = 0
 
-        if (Math.abs(dist) < 20) VP_SCROLL_GOAL = undefined
+        if (Math.abs(dist) < 20)
+            VP_SCROLL_GOAL = undefined
     }
     else {
         VP_SCROLL_SPEED *= VP_SCROLL_MOMENTUM
         VP_SCROLL_POS += VP_SCROLL_SPEED
+        VP_SMOOTH_SCROLL_POS += (VP_SCROLL_POS - VP_SMOOTH_SCROLL_POS) * VP_DATA.smoothScroll
     }
 
-    scrollTo(0, VP_SCROLL_POS)
+    scrollTo(0, VP_SMOOTH_SCROLL_POS)
 
     requestAnimationFrame(VP_UPDATE)
 }
@@ -122,30 +139,41 @@ function VP_UPDATE() {
 // -- Smooth scrolling --
 let VP_LAST_SCROLL_POS = scrollY
 let VP_SCROLL_POS = scrollY
+let VP_SMOOTH_SCROLL_POS = scrollY
 let VP_SCROLL_SPEED = 0
 let VP_SCROLL_GOAL = undefined
 
+// Disable native scrolling
+addEventListener('scroll', e => {e.preventDefault()}, {passive: false})
 addEventListener('wheel', e => {
-    VP_SCROLL_SPEED = e.deltaY * VP_SCROLL_FORCE
+    if (Math.abs(e.deltaY) > 2) {
+        // Don't slow down
+        const speed = e.deltaY * VP_SCROLL_FORCE
+        if (Math.abs(VP_SCROLL_SPEED) > Math.abs(speed)) return
+
+        VP_SCROLL_SPEED = speed
+    }
+    else VP_SCROLL_SPEED = 0
 })
 
 // -- Move to anchor tags --
 document.addEventListener('click', function(e) {
     const anchor = e.target.closest('a')
+    if (!anchor) return
+
     const href = anchor.getAttribute('href')
+    if (!href) return
 
-    if (anchor && href) {
-        const hash = href.startsWith('#')
-        if (hash) {
-            if (href == '#') VP_SCROLL_GOAL = 0
-            else {
-                const targetId = anchor.getAttribute('href')
-                const targetElement = document.querySelector(targetId)
+    const hash = href.startsWith('#')
+    if (!hash) return
 
-                if (targetElement) {
-                    VP_SCROLL_GOAL = targetElement.getBoundingClientRect().top + scrollY
-                }
-            }
+    if (href == '#') VP_SCROLL_GOAL = 0
+    else {
+        const targetId = anchor.getAttribute('href')
+        const targetElement = document.querySelector(targetId)
+
+        if (targetElement) {
+            VP_SCROLL_GOAL = targetElement.getBoundingClientRect().top + scrollY
         }
     }
 })
